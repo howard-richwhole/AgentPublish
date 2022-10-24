c09c1af33794bc665e7e7139401bca6982373054
Howard Yeh <howard@richwhole.net>
2022-10-24 09:44:18 +0800

    change deploy folder name

--- main.mjs
+++ main.mjs
@@ -75,7 +75,7 @@ function copyFolder(target) {
     data.match(/(?<=\<PublishUrl\>).*(?=\<\/PublishUrl\>)/)[0]
   )
   const hash = execSync('git rev-parse HEAD', { cwd }).toString()
-  fs.copySync(publishFolder, path.resolve(output, `HongBo.Agent`))
+  fs.copySync(publishFolder, path.resolve(output, `HongBo.AgentWeb`))
   fs.writeFileSync(
     path.resolve(output, target + '.txt'),
     `${target}\nHongBo.AgentWeb:${hash}`
